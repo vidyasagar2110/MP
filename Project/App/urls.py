f8a78afa12ca0f0bb73d9ab5474a95b5455a6132
vidyasagar2110 <tvidyasagar2110@gmail.com>
2025-01-23 23:03:57 +0530

    git

--- Project/App/urls.py
+++ Project/App/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
-urlpatterns=[
- path('', views.home, name='home')
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('graphical-method/', views.graphical_method, name='graphical_method'),
+    path('simplex-method/', views.simplex_method, name='simplex_method'),
 ]
--- Project/App/views.py
+++ Project/App/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+
 def home(request):
- return render(request,'home.html',{'name':'Sagar'})
+    return render(request, 'home.html')
+
+def graphical_method(request):
+    return render(request, 'graphical_method.html')
+
+def simplex_method(request):
+    return render(request, 'simplex_method.html')
