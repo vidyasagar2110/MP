54acf18b978d2a29b5326d74d822d379e749f7fa
vidyasagar2110 <tvidyasagar2110@gmail.com>
2025-01-25 12:26:57 +0530

    git

--- Project/App/urls.py
+++ Project/App/urls.py
@@ -2,14 +2,18 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Home page
     path('', views.home, name='home'),
+
+    # Graphical Method
     path('graphical/', views.graphical_method, name='graphical_method'),
     path('graphical/steps/', views.graphical_steps, name='graphical_steps'),
     path('graphical/solve/', views.graphical_solve, name='graphical_solve'),
     path('graphical/application/', views.graphical_application, name='graphical_application'),
-    
+
+    # Simplex Method
     path('simplex/', views.simplex_method, name='simplex_method'),
     path('simplex/steps/', views.simplex_steps, name='simplex_steps'),
-    path('simplex/solve/', views.simplex_solve, name='simplex_solve'),
+    path('simplex/solve/', views.simplex_solver, name='simplex_solver'),  # Updated to avoid conflict
     path('simplex/application/', views.simplex_application, name='simplex_application'),
 ]
